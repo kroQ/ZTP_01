9dca96642f4c0b8a1c12a9fa8a1cc6d19393c4cf
Mateusz Krok <mateusz.krok@comarch.pl>
2017-03-10 15:14:30 +0100

    Zrobione w comarchu

--- src/Client.java
+++ src/Client.java
@@ -8,22 +8,38 @@ import java.io.IOException;
  * @author mati
  */
 public class Client {
-        public static void main(String[] args) {
-            //System.out.println(args[0] + args[1]);
-            
-            Levenshtein lev = new Levenshtein();
-            
-            String imie = "imie1";
-            
-        String fileName = "./folder/file.txt";
+
+    public static void main(String[] args) {
+        //System.out.println(args[0] + args[1]);
+
+        Levenshtein lev = new Levenshtein();
+
+//        String imie = "imie1";
+        String imie = args[1];
+        int linia = 0;
+
+        //String fileName = "./folder/file.txt";
+        String fileName = args[0];
         BufferedReader reader = null;
         try {
             reader = new BufferedReader(new FileReader(fileName));
             String tmp;
+            int min = 999_999_999;
+            int tmpInt = 0;
             while ((tmp = reader.readLine()) != null) {
-                System.out.println(tmp);
-                System.out.println(lev.levenshteinDistance(imie, tmp));
+                linia++;
+                //System.out.println(tmp);
+                //System.out.println(lev.levenshteinDistance(imie, tmp.trim()));
+                tmpInt = lev.levenshteinDistance(imie, tmp.trim());
+                if (min == 0) {
+                    System.out.println("Linia : " + linia);
+                    return;
+                }
+                if (min > tmpInt) {
+                    min = tmpInt;
+                }
             }
+            System.out.println(min);
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -33,5 +49,6 @@ public class Client {
                 e.printStackTrace();
             }
         }
+        
     }
 }
